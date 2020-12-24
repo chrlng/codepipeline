e3d2e777011f8763af4e34d26b5a8c0d573252b4
Christine Nguyen <christine_nguyenl@berkeley.edu>
2020-12-23 20:40:32 -0800

    trying pipeline

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_glue_job" "example" {
+  name     = "example"
+  role_arn = "arn:aws:iam::288051765299:role/AWSGlueServiceRoleDefault"
+
+  command {
+    script_location = "s3://aws-glue-scripts-288051765299-us-west-2/christine_nguyenl@berkeley.edu/codepipeline-test"
+  }
+}
